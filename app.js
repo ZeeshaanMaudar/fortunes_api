577d3d835e7517dbc30ad77ad2769f63f020c8e1
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-10 16:05:56 +0200

    change name of file from index to app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const fortunes = require('./data/fortunes.json');
 
-const port = 3000;
-
 const app = express();
 
 app.get('/fortunes', (req, res) => {
   res.json(fortunes);
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+module.exports = app;
