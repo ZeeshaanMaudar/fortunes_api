54af152b2a30bf8b0aa17d1d139b5bf133959f89
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-10 15:55:19 +0200

    making request for fortunes json

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const fortunes = require('./data/fortunes.json');
 
 const port = 3000;
 
 const app = express();
 
 app.get('/fortunes', (req, res) => {
-  res.send('requesting fortunes');
+  res.json(fortunes);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
