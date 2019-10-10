4da8c70a831cd3ccbba1dd86937f2a97e15ad89e
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-10-10 16:23:40 +0200

    generate random fortunes from json

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ app.get('/fortunes', (req, res) => {
   res.json(fortunes);
 });
 
+app.get('/fortunes/random', (req, res) => {
+  console.log('requesting random fortunes');
+
+  const random_index = Math.floor(Math.random() * fortunes.length);
+  
+  const random_fortune = fortunes[random_index];
+  res.json(random_fortune);
+});
+
 module.exports = app;
